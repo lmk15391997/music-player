036a67ea327a87e21cfda7f45fcb382a4e16eff1
lmk15391997 <598812989@qq.com>
2019-11-15 10:35:46 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <player />
+  </div>
+</template>
+
+<script>
+import Player from '../components/Player.vue'
+export default {
+ components : {
+     Player
+ },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.home{
+    position: relative;
+    z-index: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width:100%;
+    height: 100%;
+    background-color: #ffedff;
+}
+</style>
